194b42de8bb2262f06bd5550332b3157f7b6bbf1
P4T013123 <patodadh@gmail.com>
2025-04-28 04:17:33 +0000

    main

--- vite-project/src/Componentes/Aleatorios/index.jsx
+++ vite-project/src/Componentes/Aleatorios/index.jsx
@@ -3,13 +3,21 @@ import './style.css';
 
 function Aleatorios({ agregarFavorito }) {
   const [preguntas, setPreguntas] = useState([]);
+  const [preguntasFiltradas, setPreguntasFiltradas] = useState([]);
+  const [dificultadSeleccionada, setDificultadSeleccionada] = useState('');
+  const [inputBusqueda, setInputBusqueda] = useState('');
 
   const cargarPreguntas = () => {
     fetch('https://opentdb.com/api.php?amount=10')
       .then(response => response.json())
       .then(data => {
         console.log(data);
-        setPreguntas(data.results);
+        const preguntasConId = data.results.map((pregunta, index) => ({
+          ...pregunta,
+          id: index + 1
+        }));
+        setPreguntas(preguntasConId);
+        setPreguntasFiltradas(preguntasConId);
       });
   };
 
@@ -17,23 +25,61 @@ function Aleatorios({ agregarFavorito }) {
     cargarPreguntas();
   }, []);
 
+  const filtrarPreguntas = (texto, dificultad = dificultadSeleccionada) => {
+    const textoMinuscula = texto.toLowerCase();
+  
+    const resultado = preguntas.filter(p =>
+      (p.id.toString().includes(textoMinuscula) || p.question.toLowerCase().includes(textoMinuscula)) &&
+      (dificultad === '' || p.difficulty === dificultad)
+    );
+  
+    setPreguntasFiltradas(resultado);
+  };
+  
+
   return (
     <div className="contenedor">
       <h1>Preguntas Aleatorias</h1>
       <button className="boton-recargar" onClick={cargarPreguntas}>🔄 Cargar Nuevas Preguntas</button>
-      {preguntas && preguntas.length > 0 ? (
-        preguntas.map((pregunta, index) => (
+      
+      <input
+  type="text"
+  placeholder="Buscar pregunta por ID o texto..."
+  value={inputBusqueda}
+  onChange={(e) => {
+    setInputBusqueda(e.target.value);
+    filtrarPreguntas(e.target.value, dificultadSeleccionada);
+  }}
+  className="input-buscador"
+/>
+
+      {preguntasFiltradas.length > 0 ? (
+        preguntasFiltradas.map((pregunta, index) => (
           <div key={index} className="carta">
             <h3 dangerouslySetInnerHTML={{ __html: pregunta.question }} />
             <button onClick={() => agregarFavorito(pregunta)}>⭐ Favorito</button>
+            <select
+  value={dificultadSeleccionada}
+  onChange={(e) => {
+    setDificultadSeleccionada(e.target.value);
+    filtrarPreguntas(inputBusqueda, e.target.value); // también filtramos al cambiar
+  }}
+  className="select-dificultad"
+>
+  <option value="">Todas las dificultades</option>
+  <option value="easy">Fácil</option>
+  <option value="medium">Medio</option>
+  <option value="hard">Difícil</option>
+</select>
           </div>
         ))
       ) : (
-        <p>Cargando preguntas...</p>
+        <p>No se encontraron preguntas.</p>
       )}
     </div>
   );
 }
 
 export default Aleatorios;
+
 
